5bb91152f16796f4079248fc104ac5ba20c7c9a5
cristian chable <cristian.chable@hotmail.com>
2022-09-02 22:24:41 -0500

    pages home controller

---
+++ test/integration/pages_test.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class PagesTest < ActionDispatch::IntegrationTest
+  test 'Should get home' do
+    get pages_home_url
+    assert_response :success
+  end
+
+  test 'should get root' do
+    get root_url
+    assert_response :success
+  end
+end

dad8405831bfd7ae6aeb79007d4e77bc8ed25bdd
cristian chable <cristian.chable@hotmail.com>
2022-10-17 20:12:43 -0500

    change email by description in recipes table

--- db/migrate/20220927011842_create_recipes.rb
+++ db/migrate/20220927011842_create_recipes.rb
@@ -2,7 +2,7 @@ class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
       t.string :name
-      t.text :email
+      t.text :description
       t.timestamps
     end
   end
